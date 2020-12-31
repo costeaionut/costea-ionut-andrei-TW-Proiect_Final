ffe5ab4b51771703d55f4c5b8ac8e84a43261b54
costeaionut <costeacionut@gmail.com>
2020-12-31 13:11:37 +0200

    add_modules

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var app = express();
 
+var mysql = require('mysql')
+
 // set the port we are listening on
 var port = process.env.PORT || 8000;
 
@@ -10,6 +12,18 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 // use res.render to load up an ejs view file
+
+var conexiune = mysql.createConnection({
+    host: "eu-cdbr-west-03.cleardb.net",
+    user: "be5df639565f3a",
+    password: "69da649c",
+    database: "heroku_a07b157368f36ae"
+});
+
+conexiune.connect(function(err){
+    if(err) throw err;
+    console.log("Lesgo");
+});
 
 // index page
 app.get('/', function(req, res) {
